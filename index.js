d1742e644000d07c598b5c39517793a972f0c3ce
игорь <gasper.tan@yandex.ru>
2023-10-20 13:58:31 +0300

    task1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getSum(a, b) {
+  const result = a + b;
+  return result;
+}
+function getSmallestNumber(a, b) {
+  const result = Math.min(a, b);
+  return result;
+}
+console.log(getSmallestNumber(100, 10));
+function castBoolean() {}
 
+function capitalize() {}
+
+function countCapitalLetters() {}
+
+export {
+  getSum, getSmallestNumber, castBoolean, capitalize, countCapitalLetters,
+};
