092c9f1ad47c5ca641e5aa7bf630353eaaed5539
игорь <gasper.tan@yandex.ru>
2023-10-24 11:01:11 +0300

    task3

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ function castBoolean(value) {
   const result = Boolean(value);
   return result;
 }
-function capitalize() {}
-
+function capitalize(str, num) {
+  if (num < 0 || num >= str.length()) {
+    return str;
+  }
+  return str.substring(0, num) + Character.toUpperCase(str.charAt(num)) + str.substring(num + 1);
+}
 function countCapitalLetters() {}
 
 export {
