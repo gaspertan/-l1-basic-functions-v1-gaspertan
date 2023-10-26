6ffa1d6ec17a8909f371d15337baa896e1845cbc
игорь <gasper.tan@yandex.ru>
2023-10-26 11:32:28 +0300

    task4

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ function castBoolean(value) {
   const result = Boolean(value);
   return result;
 }
-function capitalize(str, num) {
-  if (num < 0 || num >= str.length()) {
-    return str;
-  }
-  return str.substring(0, num) + Character.toUpperCase(str.charAt(num)) + str.substring(num + 1);
+function capitalize() {
 }
 function countCapitalLetters() {}
 
